203f33da26a83845fdfc8512c21158f1b51f1e8e
celequant <celequant@gmail.com>
2026-04-24 11:03:46 +0800

    update env.rs->env_exmaple.rs

---
+++ sht20_example/src/env_example.rs
@@ -0,0 +1,8 @@
+pub const AP_SSID: &str = "";
+pub const AP_PWD: &str = "";
+pub const ID: &str = "";
+pub const MQTT_NODE: &str = "";
+pub const MQTT_USER: &str = "";
+pub const MQTT_PWD: &str = "";
+pub const WIFI_NAME: &str = "";
+pub const WIFI_PWD: &str = "";
